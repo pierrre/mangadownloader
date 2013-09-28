680279670c30f44f20b8247486d7608386de737f
Pierre Durand <pierredurand@gmail.com>
2013-09-28 21:47:38 +0200

    fjhsfsjdkhfsjkhfskdjhsjkhdfjkh

--- service_mangafox_test.go
+++ service_mangafox_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 var (
-	serviceMangaFoxTestUrlManga, _   = url.Parse("http://mangafox.me/manga/bokurano/")
-	serviceMangaFoxTestUrlChapter, _ = url.Parse("http://mangafox.me/manga/bokurano/c010/1.html")
+	serviceMangaFoxTestUrlManga, _   = url.Parse("http://mangafox.me/manga/naruto/")
+	serviceMangaFoxTestUrlChapter, _ = url.Parse("http://mangafox.me/manga/naruto/v63/c600/1.html")
 )
 
 func getTestMangaFoxService() *MangaFoxService {
@@ -20,11 +20,11 @@ func getTestMangaFoxService() *MangaFoxService {
 func TestMangaFoxServiceManga(t *testing.T) {
 	service := getTestMangaFoxService()
 
-	testCommonServiceManga(t, service, serviceMangaFoxTestUrlManga, "Bokurano")
+	testCommonServiceManga(t, service, serviceMangaFoxTestUrlManga, "Naruto")
 }
 
 func TestMangaFoxServiceChapter(t *testing.T) {
 	service := getTestMangaFoxService()
 
-	testCommonServiceChapter(t, service, serviceMangaFoxTestUrlChapter, "010")
+	testCommonServiceChapter(t, service, serviceMangaFoxTestUrlChapter, "600")
 }
