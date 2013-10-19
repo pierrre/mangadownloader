b0d357e00f307d7c73374109e55304ded2437dcb
Pierre Durand <pierredurand@gmail.com>
2013-10-20 00:08:59 +0200

    MultiError now returns an empty string if empty

--- multierror.go
+++ multierror.go
@@ -7,6 +7,10 @@ import (
 type MultiError []error
 
 func (errs MultiError) Error() string {
+	if len(errs) == 0 {
+		return ""
+	}
+
 	buffer := new(bytes.Buffer)
 	for i, err := range errs {
 		if i >= 1 {
